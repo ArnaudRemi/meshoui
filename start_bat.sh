c4e2bd10ae46343e2a1a5496e685454a1c584685
arnaudremi <remiarnaud.duc@gmail.com>
2015-04-07 20:32:26 +0200

    valid mac address for AP

--- start_bat.sh
+++ start_bat.sh
@@ -3,7 +3,7 @@ modprobe batman-adv
 ifconfig $1 down
 
 ifconfig $1 mtu 1528
-iwconfig $1 mode ad-hoc essid meshoui ap 6d:65:73:68:6f:75 channel 1
+iwconfig $1 mode ad-hoc essid meshoui ap 02:12:34:56:78:9A channel 1
 
 batctl if add $1
 ifconfig $1 up
